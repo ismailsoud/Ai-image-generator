aee64da1d25233a097c691d30cfa5f58ef3d0a8b
ismailsoud <ismailsoudassi6@gmail.com>
2023-10-02 16:53:16 +0100

    Initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css"
+function Footer (){
+    return(
+        <div className="footer-container">
+            <div>Created By </div>
+             <span> SMAIL</span>
+        </div>
+    )
+}
+
+
+export default Footer
---
+++ src/components/imageGenerator/imageGenarator.jsx
@@ -0,0 +1,75 @@
+import './generator.css';
+import def_image from '../../images/default_image.svg'
+import { useRef, useState } from 'react';
+
+
+function ImageGenerator() {
+
+    const [image_url, setImageUrl] = useState("/")
+    const [loading, setLoading] = useState(false)
+
+    let inputRef = useRef(null)
+
+    const GeneratImage = async () =>{
+        if(inputRef.current.value===""){
+            return 0;
+        }
+        setLoading(true)
+        try{
+            const response = await fetch(
+                "https://api.openai.com/v1/images/generations",
+                {
+                    method:"POST",
+                    headers:{
+                        "Content-Type": "application/json",
+                        Authorization:
+                        "Bearer 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",
+                        "User-Agent":"Chrome",
+                    },
+                    body:JSON.stringify({
+                        prompt: `${inputRef.current.value}`,
+                        n:1,
+                        size:"512x512",
+                    }),
+                }
+            )
+            let data = await response.json();
+            console.log(data)
+            let data_array = data.data;
+            setImageUrl(data_array[0].url)
+            setLoading(false)
+        }catch (error){
+            setLoading(false)
+            alert("Please no funny business")
+            if (error.response) {
+                console.log(error.response.status);
+                console.log(error.response.data);
+              } else {
+                console.log(error.message);
+              }
+        }
+    }
+
+  return(
+    <div className='container'>
+      <h1 className='title'>Ai image <span>Generator</span></h1>
+      <div className='sub-container'> 
+        <div className='image'>
+          <img src={image_url==="/"?def_image:image_url} alt='default image'/>
+        </div>
+        <div className="loading">
+            <div className={loading?"loading-bar-full":"loading-bar"}></div>
+            <div className={loading?"loading-text":"display-none"}>Loading...</div>
+        </div>
+        <div className='input-container'>
+            <div className='sub-input-container'>
+                <input type="text" ref={inputRef} className='input'/>
+                <button onClick={()=>{GeneratImage()}}>Generat</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ImageGenerator;
